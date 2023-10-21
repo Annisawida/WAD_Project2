0553b5655b0f8ec8a9c97f2beac426ac7e28db34
Annisawida <116070462+Annisawida@users.noreply.github.com>
2023-10-21 12:05:25 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h1>selamat datang</h1>
+    <div class="container">
+        <div class="input-group mb-5">
+            <input type="number" class="form-control" placeholder = "Masukkan 2 angka (Cth : 2,15)">
+        </div>
+        <div class="input-group mb-5">
+            <select class="form-select" id="inputGroupSelect01">
+                <option selected>Pilih Operasi</option>
+                <option value="1">Penjumlahan</option>
+                <option value="2">Pengurangan</option>
+                <option value="3">Pembagian</option>
+                <option value="4">Perkalian</option>
+            </select>
+        </div>
+        <button type="button" class="btn btn-primary">Hitung</button>
+    </div>
+</body>
+</html>
